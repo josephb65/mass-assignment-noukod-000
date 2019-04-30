d8480728230b010ce790e42820a273f5d9d033dd
Billy Joseph <ingbillyjoseph@gmail.com>
2019-04-30 15:37:51 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,8 +4,8 @@ class Person
                 :weight, :handed, :complexion, :t_shirt_size,
                 :wrist_size, :glove_size, :pant_length, :pant_width
 
-  def initialize(attributes)
-    attributes.each do |key, value|
+  def initialize(att)
+    att.each do |key, value|
       self.send(("#{key}="), value)
     end
   end
